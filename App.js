063681a554b84ac30be75baaa49feee3a18b6873
benoitgoupilleau <benoit.goupilleau@gmail.com>
2019-01-25 19:39:36 +0100

    Added redux persist

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 import AppNavigator from './src/navigation/AppNavigator'
 
-import { store } from './src/redux/store/store';
+import { store, persistor } from './src/redux/store/store';
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={store} >
-        <AppNavigator />
+        <PersistGate persistor={persistor}>
+          <AppNavigator />
+        </PersistGate>
       </Provider>
     )
   }
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,21 +1,31 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { ENV } from 'react-native-dotenv';
 import thunk from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 
 import newsReducer from '../reducers/news';
 import offlineReducer from '../reducers/offline'
 
 const composeEnhancers = ENV && ENV === 'dev' ? (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose) : compose;
 
+const persistConfig = {
+  key: 'root',
+  storage: storage,
+  stateReconciler: autoMergeLevel2
+};
 
 const rootReducer = combineReducers({
   news: newsReducer,
   network: offlineReducer
 });
 
+const pReducer = persistReducer(persistConfig, rootReducer);
 
-export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+export const store = createStore(pReducer, composeEnhancers(applyMiddleware(thunk)));
 
+export const persistor = persistStore(store);
 
 
 
