d72bba48b40e41ce309edaa4c6385682f5dfbeee
benoitgoupilleau <benoit.goupilleau@gmail.com>
2019-03-02 19:37:41 +0100

    Updated ftp service

--- src/components/business/Picture.js
+++ src/components/business/Picture.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
-import { TouchableOpacity, Image } from 'react-native';
+import { TouchableOpacity, Image, Dimensions } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
-
 import Layout from '../../constants/Layout';
+
+const { width } = Dimensions.get('window');
+const pictureWidth = Math.round(width / 2) - 50
 
 const PictureWrapper = styled(TouchableOpacity)`
   margin: ${Layout.space.small};
   flex-direction: row;
   display: flex;
   justify-content: space-between;
-  width: 200px;
+  width: ${pictureWidth}px;
   position: relative;
 `;
 
 const ImageFrame = styled(Image)`
   width: 100%;
-  height: 100px;
+  height: ${Math.round(pictureWidth * 3 / 4)}px;
 `;
 
 const Icons = styled(Ionicons)`
--- src/services/ftp.js
+++ src/services/ftp.js
@@ -1,6 +1,9 @@
 import FTP from 'react-native-ftp';
-import { FTP_URL, FTP_PORT } from 'react-native-dotenv';
+import { FTP_URL_OUT, FTP_URL_HOME, FTP_PORT } from 'react-native-dotenv';
 
-FTP.setup(FTP_URL, parseInt(FTP_PORT, 10));
+export const setUpFTPOut = () => FTP.setup(FTP_URL_OUT, parseInt(FTP_PORT, 10));
+
+export const setUpFTPHome = () => FTP.setup(FTP_URL_HOME, parseInt(FTP_PORT, 10));
+
 
 export default FTP;
