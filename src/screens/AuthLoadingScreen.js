a102004fe0df8b3c3adbf72614b037508216d256
Goupilleau <benoit.goupilleau@gmail.com>
2019-09-01 21:40:20 +0200

    Refactored AuthloadingScreen

--- src/screens/AuthLoadingScreen.js
+++ src/screens/AuthLoadingScreen.js
@@ -4,30 +4,11 @@ import { connect } from 'react-redux';
 import { ActivityIndicator } from 'react-native';
 import Main from '../components/Main';
 
-import { connectDbOut, connectDbHome } from '../redux/actions/network';
-import { getDocs } from '../redux/actions/business'
-import { downloadModels } from '../redux/actions/user'
-
 import { checkAccessStorage } from '../utils/permissionsAndroid'
 
-const  AuthLoadingScreen = ({token,
-  mssqlConnected,
-  mssqlConnectionFailed,
-  connectDbOut,
-  connectDbHome,
-  connectedHome,
-  navigation,
-  downloadModels,
-  modeleDocs
-  }) => {
+const  AuthLoadingScreen = ({token, navigation }) => {
   checkAccessStorage();
-  if (!mssqlConnected && !mssqlConnectionFailed) {
-    connectedHome ? connectDbHome() : connectDbOut();
-    navigation.navigate(token !== '' ? 'App' : 'Auth');
-  } else {
-    downloadModels(modeleDocs)
-    navigation.navigate(token !== '' ? 'App' : 'Auth');
-  }
+  navigation.navigate(token !== '' ? 'App' : 'Auth');
   return (
     <Main>
       <ActivityIndicator />
@@ -38,27 +19,10 @@ const  AuthLoadingScreen = ({token,
 AuthLoadingScreen.propTypes = {
   navigation: PropTypes.object.isRequired,
   token: PropTypes.string.isRequired,
-  mssqlConnected: PropTypes.bool.isRequired,
-  mssqlConnectionFailed: PropTypes.bool.isRequired,
-  connectDbOut: PropTypes.func.isRequired,
-  connectDbHome: PropTypes.func.isRequired,
-  connectedHome: PropTypes.bool.isRequired,
-  downloadModels: PropTypes.func.isRequired,
-  modeleDocs: PropTypes.array.isRequired,
-  editedDocs: PropTypes.array.isRequired,
-  downloadedBusiness: PropTypes.array.isRequired,
-  docs: PropTypes.array.isRequired
 }
 
 const mapStateToProps = state => ({
   token: state.user.bearerToken,
-  editedDocs: state.user.editedDocs,
-  downloadedBusiness: state.user.downloadedBusiness,
-  mssqlConnected: state.network.mssqlConnected,
-  mssqlConnectionFailed: state.network.mssqlConnectionFailed,
-  modeleDocs: state.business.modeles,
-  docs: state.business.docs,
-  connectedHome: state.network.connectedHome
 })
 
-export default connect(mapStateToProps, { connectDbOut, connectDbHome, getDocs, downloadModels })(AuthLoadingScreen);
+export default connect(mapStateToProps)(AuthLoadingScreen);
