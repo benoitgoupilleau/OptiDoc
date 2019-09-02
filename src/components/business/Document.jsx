9f2414b4fd9f8f51680f01b940a1bcf577175582
Goupilleau <benoit.goupilleau@gmail.com>
2019-09-02 18:05:29 +0200

    Refactored Document

--- src/components/business/Document.jsx
+++ src/components/business/Document.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { ActivityIndicator, Dimensions, Alert, ToastAndroid } from 'react-native';
@@ -22,17 +22,16 @@ const { width } = Dimensions.get('window');
 
 import { DocumentWrapper, File, Title, StyledInput, IconsWrapper, EditIcons, Icons } from './Document.styled'
 
-class Document extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isDownloaded: true,
-      localFileName: props.fileName
-    }
-  }
+const Document = React.memo((props) => {
+  const { name, navigation, type, id, extension, dossier1, dossier2, dossier3, reviewed, locked, userId, loadingBusiness, loadingFiles, fileToDownload, uploadingDocs, prep, rea, editedDocs, fileName, prepared, isNew, modeleDownloaded, isConnected, downloadBusiness, editFile, uploadFile, uploadingFile, removeFromEdit, downLoadOneFile, updatePrepared, editPrepare, removePrepare, removeNewDoc, createFile, updateDocName } = props;
+  const [isDownloaded, setIsDownloaded] = useState(true);
+  const [localFileName, setlocalFileName] = useState(fileName);
 
-  onEdit = async () => {
-    const { type, id, extension, dossier1, userId, loadingBusiness, prep, rea, editedDocs, dossier3 } = this.props;
+  useEffect(() => {
+    checkIfDownloaded()
+  }, [])
+
+  const onEdit = async () => {
     const isEdited = editedDocs.filter(e => e.id === id).length > 0;
     if (isEdited) {
       await openFile(id, extension);
@@ -40,13 +39,13 @@ class Document extends React.Component {
       const filePath = `${rootDir}/${userId}/${dossier1}/${type}/${id}.${extension}`;
       const fileExists = await RNFS.exists(filePath);
       if (fileExists) {
-        this.props.editFile({ id, editPath: `${EXTERNAL_PATH}${id}.${extension}`, affaire: dossier1, extension, dossier3 }, filePath)
+        editFile({ id, editPath: `${EXTERNAL_PATH}${id}.${extension}`, affaire: dossier1, extension, dossier3 }, filePath)
       } else if (!loadingBusiness.includes(dossier1)) {
-        if (this.props.modeleDownloaded === 'in progress') {
+        if (modeleDownloaded === 'in progress') {
           Alert.alert('Modèle en cours de téléchargement', 'Les fichiers modèles sont en cours de téléchargement. Merci de réessayer dans quelques instants', [{ text: 'Ok' }]);
         } else {
-          if (this.props.isConnected) {
-            this.props.downloadBusiness(userId, dossier1, prep, rea)
+          if (isConnected) {
+            downloadBusiness(userId, dossier1, prep, rea)
           } else {
             Alert.alert('Vous êtes en mode hors-ligne', 'Vous pourrez télécharger cette affaire une fois votre connexion rétablie', [{ text: 'Ok' }]);
           }
@@ -55,14 +54,13 @@ class Document extends React.Component {
     }
   }
 
-  confirmedOnUpload = async () => {
-    const { id, extension, isNew, userId, dossier1, type } = this.props;
+  const confirmedOnUpload = async () => {
     const filePath = `${EXTERNAL_PATH}${id}.${extension}`;
     const destPath = `${rootDir}/${userId}/${dossier1}/${type}/${id}.${extension}`;
-    const file = pick(this.props, Tables.docField);
-    const userName = this.props.name;
+    const file = pick(props, Tables.docField);
+    const userName = name;
     const now = new Date();
-    const date = now.getFullYear() + '-' + (now.getMonth() + 1).toLocaleString('fr-FR', { minimumIntegerDigits: 2 }) + '-' + now.getDate().toLocaleString('fr-FR', { minimumIntegerDigits: 2 })
+    const date = `${now.getFullYear()}-${(now.getMonth() + 1).toLocaleString('fr-FR', { minimumIntegerDigits: 2 })}-${now.getDate().toLocaleString('fr-FR', { minimumIntegerDigits: 2 })}`
     const fileToUpLoad = {
       ...file,
       upLoadedOn: date,
@@ -76,16 +74,15 @@ class Document extends React.Component {
       await RNFS.unlink(`${EXTERNAL_PATH}${id}(0).${extension}`)
     }
     await RNFS.copyFile(filePath, destPath);
-    this.props.uploadingFile(id);
+    uploadingFile(id);
     if (isNew) {
-      return this.props.createFile(filePath, fileToUpLoad)
+      return createFile(filePath, fileToUpLoad)
     } else {
-      return this.props.uploadFile(filePath, fileToUpLoad);
+      return uploadFile(filePath, fileToUpLoad);
     }
   }
 
-  onUpload = () => {
-    const { isConnected, prepared } = this.props;
+  const onUpload = () => {
     if (isConnected) {
       if (prepared === 'O') {
         Alert.alert("Confirmer l'envoi", "Etes-vous sûr de vouloir envoyer ce fichier ?", [{
@@ -93,7 +90,7 @@ class Document extends React.Component {
           style: 'cancel',
         }, {
           text: 'Oui',
-          onPress: () => this.confirmedOnUpload()
+          onPress: () => confirmedOnUpload()
         }]);
       } else {
         Alert.alert('Envoi impossible', "Le fichier n'est pas coché comme 'Préparé'", [{ text: 'Ok' }]);
@@ -103,56 +100,44 @@ class Document extends React.Component {
     }
   }
 
-  confirmedOnPrepare = (newPrepared, now) => {
+  const confirmedOnPrepare = (newPrepared, now) => {
     const PreparedOn = now.getFullYear() + '-' + (now.getMonth() + 1).toLocaleString('fr-FR', { minimumIntegerDigits: 2 }) + '-' + now.getDate().toLocaleString('fr-FR', { minimumIntegerDigits: 2 })
     const Revisable = 'O';
-    this.props.updatePrepared(this.props.id, newPrepared, PreparedOn, this.props.name, Revisable);
-    this.props.editPrepare({ id: this.props.id, prepared: true, affaire: this.props.dossier1, extension: this.props.extension, dossier3: this.props.dossier3 })
+    updatePrepared(id, newPrepared, PreparedOn, name, Revisable);
+    editPrepare({ id: id, prepared: true, affaire: dossier1, extension: extension, dossier3: dossier3 })
   }
 
-  onPrepare = () => {
-    const newPrepared = this.props.prepared === 'O' ? 'N' : 'O';
+  const onPrepare = () => {
+    const newPrepared = prepared === 'O' ? 'N' : 'O';
     const now = new Date();
     if (newPrepared === 'N') {
       const PreparedOn = '1900-01-01';
       const Revisable = 'N';
       const PreparedBy = '';
-      this.props.updatePrepared(this.props.id, newPrepared, PreparedOn, PreparedBy, Revisable);
-      this.props.removePrepare(this.props.id)
+      updatePrepared(id, newPrepared, PreparedOn, PreparedBy, Revisable);
+      removePrepare(id)
     } else {
       Alert.alert("Confirmer la préparation", "Etes-vous sûr de vouloir cocher ce fichier comme préparé ?", [{
         text: 'Annuler',
         style: 'cancel',
       }, {
         text: 'Oui',
-        onPress: () => this.confirmedOnPrepare(newPrepared, now)
+        onPress: () => confirmedOnPrepare(newPrepared, now)
       }]);
     }
   }
 
-  onCancel = () => {
-    Alert.alert(
-      'Etes-vous sûr de vouloir annuler les modifications ?',
-      'Les modifications seront perdues définitivement',
-      [
-        { text: 'Annuler', style: 'cancel'},
-        { text: 'Oui', onPress: this.removeFile },
-      ],
-    )
-  }
-
-  removeFile = async () => {
-    const { id, isNew, userId, dossier1, extension, type } = this.props;
+  const removeFile = async () => {
     if (!isNew) {
-      const doc = this.props.editedDocs.filter(e => e.id === id)[0]
+      const doc = editedDocs.filter(e => e.id === id)[0]
       if (doc.prepared) {
-        this.props.updatePrepared(id, 'N', '1900-01-01', '');
+        updatePrepared(id, 'N', '1900-01-01', '');
       }
-      this.props.removeFromEdit(id);
+      removeFromEdit(id);
     } else {
-      this.props.removeNewDoc(id);
-      this.props.removeFromEdit(id);
-      const localPath= `${rootDir}/${userId}/${dossier1}/${type}/${id}.${extension}`
+      removeNewDoc(id);
+      removeFromEdit(id);
+      const localPath = `${rootDir}/${userId}/${dossier1}/${type}/${id}.${extension}`
       await RNFS.unlink(localPath)
       const externalPath = `${EXTERNAL_PATH}${id}.${extension}`;
       try {
@@ -164,47 +149,20 @@ class Document extends React.Component {
     }
   }
 
-  onOpenFile = async () => {
-    const { fileName, type, navigation, id, dossier3, extension, dossier1, dossier2, userId, loadingBusiness, editedDocs, prepared, reviewed, locked} = this.props;
-    const isEdited = editedDocs.filter(e => e.id === id && !!e.editPath).length > 0;
-    const isPrepared = editedDocs.filter(e => e.id === id && e.prepared).length > 0;
-    const isDownloaded = await RNFS.exists(`${rootDir}/${userId}/${dossier1}/${dossier2}/${id}.${extension}`)
-    if (isDownloaded) {
-      const secondVersion = await RNFS.exists(`${EXTERNAL_PATH}${id}(0).${extension}`);
-      if (secondVersion && isEdited) {
-        RNFS.copyFile(`${EXTERNAL_PATH}${id}(0).${extension}`, `${EXTERNAL_PATH}${id}.${extension}`)
-          .then(() => RNFS.unlink(`${EXTERNAL_PATH}${id}(0).${extension}`)
-            .then(() => 
-              navigation.navigate('Pdf', { title: fileName, id, dossier3, extension, dossier1, type, isEdited, prepared, reviewed, isPrepared, locked })
-            )
-          )
-          .catch((e) => {
-            Sentry.captureException(e, { func: 'onOpenFile', doc: 'Document.js'})
-            return;
-          })
-      } else {
-        return navigation.navigate('Pdf', { title: fileName, id, dossier3, extension, dossier1, type, isEdited, prepared, reviewed, isPrepared, locked })
-      } 
-    }
-    if (!loadingBusiness.includes(dossier1)) {
-      if (this.props.modeleDownloaded === 'in progress') {
-        return Alert.alert('Modèle en cours de téléchargement', 'Les fichiers modèles sont en cours de téléchargement. Merci de réessayer dans quelques instants', [{ text: 'Ok' }]);
-      } else {
-        this.onDownloadFile();
-      }
-    }
-    return ToastAndroid.showWithGravity(
-      "Affaire en cours de téléchargement",
-      ToastAndroid.SHORT,
-      ToastAndroid.CENTER,
-    );
-    
+  const onCancel = () => {
+    Alert.alert(
+      'Etes-vous sûr de vouloir annuler les modifications ?',
+      'Les modifications seront perdues définitivement',
+      [
+        { text: 'Annuler', style: 'cancel' },
+        { text: 'Oui', onPress: removeFile },
+      ],
+    )
   }
 
-  onDownloadFile = () => {
-    const { id, dossier1, extension } = this.props;
-    if (this.props.isConnected) {
-      this.props.downLoadOneFile(id, extension, dossier1)
+  const onDownloadFile = () => {
+    if (isConnected) {
+      downLoadOneFile(id, extension, dossier1)
       return ToastAndroid.showWithGravity(
         "Fichier en cours de téléchargement",
         ToastAndroid.SHORT,
@@ -215,107 +173,137 @@ class Document extends React.Component {
     }
   }
 
-  checkIfDownloaded = async () => {
-    const { id, extension, dossier1, dossier2, userId } = this.props;
-    const filepath = `${rootDir}/${userId}/${dossier1}/${dossier2}/${id}.${extension}`
-    const isDownloaded = await RNFS.exists(filepath)
-    if (isDownloaded !== this.state.isDownloaded) this.setState({ isDownloaded })
+  const onOpenFile = async () => {
+    const isEdited = editedDocs.filter(e => e.id === id && !!e.editPath).length > 0;
+    const isPrepared = editedDocs.filter(e => e.id === id && e.prepared).length > 0;
+    const isDownloaded = await RNFS.exists(`${rootDir}/${userId}/${dossier1}/${dossier2}/${id}.${extension}`)
+    if (isDownloaded) {
+      const secondVersion = await RNFS.exists(`${EXTERNAL_PATH}${id}(0).${extension}`);
+      if (secondVersion && isEdited) {
+        RNFS.copyFile(`${EXTERNAL_PATH}${id}(0).${extension}`, `${EXTERNAL_PATH}${id}.${extension}`)
+          .then(() => RNFS.unlink(`${EXTERNAL_PATH}${id}(0).${extension}`)
+            .then(() =>
+              navigation.navigate('Pdf', { title: fileName, id, dossier3, extension, dossier1, type, isEdited, prepared, reviewed, isPrepared, locked })
+            )
+          )
+          .catch((e) => {
+            Sentry.captureException(e, { func: 'onOpenFile', doc: 'Document.js' })
+            return;
+          })
+      } else {
+        return navigation.navigate('Pdf', { title: fileName, id, dossier3, extension, dossier1, type, isEdited, prepared, reviewed, isPrepared, locked })
+      }
+    }
+    if (!loadingBusiness.includes(dossier1)) {
+      if (modeleDownloaded === 'in progress') {
+        return Alert.alert('Modèle en cours de téléchargement', 'Les fichiers modèles sont en cours de téléchargement. Merci de réessayer dans quelques instants', [{ text: 'Ok' }]);
+      } else {
+        onDownloadFile();
+      }
+    }
+    return ToastAndroid.showWithGravity(
+      "Affaire en cours de téléchargement",
+      ToastAndroid.SHORT,
+      ToastAndroid.CENTER,
+    );
+
   }
 
-  displayLeftIcon = () => {
-    const { id, loadingFiles } = this.props;
-    const isDownloaded = this.state.isDownloaded
-    if (!isDownloaded || this.props.fileToDownload.includes(id)) {
+  const checkIfDownloaded = async () => {
+    const filepath = `${rootDir}/${userId}/${dossier1}/${dossier2}/${id}.${extension}`
+    const downloaded = await RNFS.exists(filepath)
+    if (isDownloaded !== downloaded) setIsDownloaded(downloaded)
+  }
+
+  const displayLeftIcon = () => {
+    if (!isDownloaded || fileToDownload.includes(id)) {
       return (
-        loadingFiles.includes(id) ? 
+        loadingFiles.includes(id) ?
           <ActivityIndicator style={{ paddingLeft: 10, paddingRight: 10 }} />
-        :
+          :
           <Icons
             name={'md-cloud-download'}
             size={Layout.icon.small}
             color={Colors.secondColor}
-            onPress={this.onDownloadFile}
+            onPress={onDownloadFile}
           />
       )
     }
     return undefined;
   }
 
-  render() {
-    this.checkIfDownloaded();
-    const { fileName, type, id, editedDocs, uploadingDocs, reviewed, prepared, locked, isNew } = this.props;
-    const isEdited = editedDocs.filter(e => e.id === id).length > 0;
-    return (
-      <DocumentWrapper
-        onPress={this.onOpenFile}
-      > 
-        <File>
-          {this.displayLeftIcon()}
-          {isNew ?
-            <StyledInput
-              width={width}
-              allowFontScaling
-              onChangeText={(localFileName) => this.setState({ localFileName })}
-              placeholder="Nom du fichier"
-              value={this.state.localFileName}
-              editable={prepared === 'N'}
-              onEndEditing={() => this.props.updateDocName(this.state.localFileName, id)}
-            /> :
-            <Title width={width}>{fileName}</Title>}
-        </File>
-        {type === Folder.rea ? 
-          <IconsWrapper>
-            {isEdited && (
-              <EditIcons>
-                <Icons
-                  name="md-close"
-                  size={Layout.icon.default}
-                  color="red"
-                  onPress={this.onCancel}
-                />
-                {!uploadingDocs.includes(id) ?
+  const isEdited = editedDocs.filter(e => e.id === id).length > 0;
+  return (
+    <DocumentWrapper
+      onPress={onOpenFile}
+    >
+      <File>
+        {displayLeftIcon()}
+        {isNew ?
+          <StyledInput
+            width={width}
+            allowFontScaling
+            onChangeText={(name) => setlocalFileName(name)}
+            placeholder="Nom du fichier"
+            value={localFileName}
+            editable={prepared === 'N'}
+            onEndEditing={() => updateDocName(localFileName, id)}
+          /> :
+          <Title width={width}>{fileName}</Title>}
+      </File>
+      {type === Folder.rea ?
+        <IconsWrapper>
+          {isEdited && (
+            <EditIcons>
+              <Icons
+                name="md-close"
+                size={Layout.icon.default}
+                color="red"
+                onPress={onCancel}
+              />
+              {!uploadingDocs.includes(id) ?
                 <Icons
                   name="md-cloud-upload"
                   size={Layout.icon.default}
                   color={Colors.secondColor}
-                  onPress={this.onUpload}
-                /> : <ActivityIndicator style={{ paddingLeft: 5, paddingRight: 5 }}/>}
-                <Icons
-                  name="md-checkbox-outline"
-                  size={Layout.icon.default}
-                  color={prepared === 'O' ? "green" : Colors.thirdColor}
-                  onPress={locked === 'O' || (prepared === 'O' && !isEdited) ? () => { } : this.onPrepare}
-                />
-              </EditIcons>
-            )}
-            {locked === 'O' ? 
+                  onPress={onUpload}
+                /> : <ActivityIndicator style={{ paddingLeft: 5, paddingRight: 5 }} />}
+              <Icons
+                name="md-checkbox-outline"
+                size={Layout.icon.default}
+                color={prepared === 'O' ? "green" : Colors.thirdColor}
+                onPress={locked === 'O' || (prepared === 'O' && !isEdited) ? () => { } : onPrepare}
+              />
+            </EditIcons>
+          )}
+          {locked === 'O' ?
             <Icons
               name="md-lock"
               size={Layout.icon.default}
-            /> : (reviewed === 'O' || (prepared === 'O' && !isEdited)) ? 
+            /> : (reviewed === 'O' || (prepared === 'O' && !isEdited)) ?
               <Icons
                 name="md-checkbox-outline"
                 size={Layout.icon.default}
                 color={"green"}
                 onPress={() => { }}
               /> :
-            <Icons
-              name="md-create"
-              size={Layout.icon.default}
-              onPress={this.onEdit}
-            />}
-          </IconsWrapper> :
-          <IconsWrapper>
-            <Icons
-              name="md-lock"
-              size={Layout.icon.default}
-            />
-          </IconsWrapper>
-        }
-      </DocumentWrapper>
-    );
-  } 
-}
+              <Icons
+                name="md-create"
+                size={Layout.icon.default}
+                onPress={onEdit}
+              />}
+        </IconsWrapper> :
+        <IconsWrapper>
+          <Icons
+            name="md-lock"
+            size={Layout.icon.default}
+          />
+        </IconsWrapper>
+      }
+    </DocumentWrapper>
+  );
+
+})
 
 Document.propTypes = {
   fileName: PropTypes.string.isRequired,
